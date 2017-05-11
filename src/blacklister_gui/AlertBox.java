615f27e8e50c43febcbc20bd1a140e5d8442186c
BrettDixonBCIT <brettdixon1@gmail.com>
2017-05-10 17:22:23 -0700

    Uploading for public viewing

---
+++ src/blacklister_gui/AlertBox.java
@@ -0,0 +1,43 @@
+package blacklister_gui;
+
+/*
+Creates an alert box with Modality.
+
+Code from Bucky Roberts : https://github.com/buckyroberts/Source-Code-from-Tutorials/blob/master/JavaFX/005_creatingAlertBoxes/AlertBox.java
+ */
+
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class AlertBox {
+
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(400);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close this window");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
+
+
